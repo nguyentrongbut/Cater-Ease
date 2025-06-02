53863d88129f936e083565d94e5048cf54a67de1
nguyentrongbut <cloly031128@gmail.com>
2025-06-03 01:21:38 +0700

    feat(ui): add header and reponsive header

---
+++ src/components/button/button.sheet.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Link from "next/link";
+import {SheetClose} from "@/components/ui/sheet";
+
+const ButtonSheet = ({href, number = 0, children}: {href:string, number?:number, children:React.ReactNode}) => {
+    return (
+        <SheetClose asChild>
+            <Link href={href} className="flex justify-between items-center">
+                {children}
+                {
+                    number > 0 && (
+                        <span className="text-xs text-primary font-bold">{number}</span>
+                    )
+                }
+            </Link>
+        </SheetClose>
+    )
+}
+
+export default ButtonSheet;

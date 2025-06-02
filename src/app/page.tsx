b5420516e9207883caaeb59f9d7a3aead49a6831
nguyentrongbut <cloly031128@gmail.com>
2025-06-02 16:16:08 +0700

    chore(taiwindcss): font with taiwindcss

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-red-600">Close issue #3</div>
-  );
-}
---
+++ src/components/layout/customer/header.customer.tsx
@@ -0,0 +1,11 @@
+import HeaderWrapper from "@/components/layout/header.wrapper";
+
+const HeaderCustomer = () => {
+    return (
+        <HeaderWrapper>
+            <div className="text-red-600 font-title">hello world</div>
+        </HeaderWrapper>
+    )
+}
+
+export default HeaderCustomer;

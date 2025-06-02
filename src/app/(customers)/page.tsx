5248b6ffd9948986090b57bbe949c8a20ccd3210
nguyentrongbut <cloly031128@gmail.com>
2025-06-02 16:28:01 +0700

    feat: add dark mode

--- src/app/(customers)/page.tsx
+++ src/app/(customers)/page.tsx
@@ -1,9 +1,11 @@
 import HeaderCustomer from "@/components/layout/customer/header.customer";
+import {ModeToggle} from "@/components/common/mode.toggle";
 
 export default function Home() {
     return (
         <>
-            <HeaderCustomer></HeaderCustomer>
+            {/*<HeaderCustomer></HeaderCustomer>*/}
+            <ModeToggle></ModeToggle>
         </>
     );
 }

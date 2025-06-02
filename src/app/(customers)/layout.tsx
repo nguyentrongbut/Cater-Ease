16f26272276808181459e757b9ed15344aaa4966
nguyentrongbut <cloly031128@gmail.com>
2025-06-02 16:16:08 +0700

    chore(tailwindcss): use Tailwind CSS font
    # Conflicts:
    #	package-lock.json
    #	src/app/(customers)/layout.tsx
    #	src/app/(customers)/page.tsx
    #	src/app/globals.css
    #	src/app/layout.tsx
    #	src/components/layout/header.wrapper.tsx
    #	tailwind.config.ts

--- src/app/(customers)/layout.tsx
+++ src/app/(customers)/layout.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import HeaderCustomer from "@/components/layout/customer/header/header.customer";
-import FooterCustomer from "@/components/layout/customer/footer/footer.customer";
 
 const CustomersLayout = ({children}: { children: React.ReactNode }) => {
     return (
-        <main className="flex flex-col min-h-screen">
-            <HeaderCustomer></HeaderCustomer>
-            <main className="flex-1">
-                {children}
-            </main>
-            <FooterCustomer></FooterCustomer>
-        </main>
+        <>
+            {children}
+        </>
     );
 }
 
--- src/components/layout/header.wrapper.tsx
+++ src/components/layout/header.wrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderWrapper = ({children}: { children: React.ReactNode }) => {
     return (
-        <header className="sticky top-0 z-50">
+        <header>
             {children}
         </header>
     )

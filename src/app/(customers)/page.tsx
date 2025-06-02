b94c2ece182dfdfca878e907dcb0e23dabfecf75
nguyentrongbut <cloly031128@gmail.com>
2025-06-03 01:21:38 +0700

    feat(ui): add header and reponsive header

--- src/app/(customers)/page.tsx
+++ src/app/(customers)/page.tsx
@@ -1,11 +1,9 @@
 import HeaderCustomer from "@/components/layout/customer/header.customer";
-import {ModeToggle} from "@/components/common/mode.toggle";
 
 export default function Home() {
     return (
         <>
-            {/*<HeaderCustomer></HeaderCustomer>*/}
-            <ModeToggle></ModeToggle>
+            <HeaderCustomer></HeaderCustomer>
         </>
     );
 }

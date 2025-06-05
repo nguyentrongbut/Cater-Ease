1b837871e35b3bae13e5d8f88ed5c1e07b29fc76
nguyentrongbut <cloly031128@gmail.com>
2025-06-06 01:09:42 +0700

    feat(auth): delete tab and add logic login signup with mock-api. Refs #6

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,27 @@
 'use client'
 import React from "react";
 import Link from "next/link"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"
 import LoginCustomer from "@/app/login/login.customer";
-import LoginCaterer from "@/app/login/login.caterer";
 import {useForm} from "react-hook-form";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import Logo from "@/components/common/logo";
+import {login} from "@/app/action";
+import {useRouter} from "next/navigation";
+import toast from "react-hot-toast";
+
 
 const formSchema = z.object({
-    email: z.string().email({ message: 'Email không hợp lệ' }),
+    email: z.string().email({message: 'Email không hợp lệ'}),
     password: z.string().min(1, 'Mật khẩu không được để trống'),
 });
 
 export type LoginForm = z.infer<typeof formSchema>;
 
 export default function LoginPage() {
+
+    const router = useRouter();
 
     // 1. Define your form.
     const form = useForm<LoginForm>({
@@ -29,58 +33,53 @@ export default function LoginPage() {
     });
 
     // 2. Define a submit handler.
-    async function onSubmit(values: LoginForm, userType: "customer" | "caterer") {
-        console.log({...values, userType});
+    async function onSubmit(values: LoginForm) {
+        try {
+            const result = await login(values);
+            if (!result) return toast.error('Login failed. Please check your credentials!');
+            const role = result[0]?.role || '';
+            if (result) {
+                localStorage.setItem(
+                    'userInfo',
+                    JSON.stringify(result)
+                );
+                // Trigger event
+                window.dispatchEvent(new Event('userInfoChanged'));
+            }
+
+            toast.success('Login successful!');
+            if (role !== 'caterer') return router.push('/')
+            return router.push('/dashboard/caterer');
+        } catch (error) {
+            console.error('Login error:', error);
+        }
     }
 
 
     return (
-        <div className="min-h-screen bg-gradient-to-br from-orange-50 to-red-50 flex items-center justify-center p-4">
+        <div
+            className="min-h-screen bg-rose-50 dark:bg-background flex items-center justify-center p-4">
             <div className="w-full max-w-md">
-                <div className="flex justify-center flex-col items-center mb-8">
-                    <Logo size="xl"></Logo>
-                    <p className="text-gray-600 mt-2">Welcome back! Please sign in to your account.</p>
+                <div className="text-center mb-8">
+                    <div className="flex justify-center">
+                        <Logo size="xl"></Logo>
+                    </div>
+                    <p className="text-gray-600 mt-2 dark:text-white">Welcome back! Please sign in to your account.</p>
                 </div>
-
-                <Tabs defaultValue="customer" className="w-full">
-                    <TabsList className="grid w-full grid-cols-2">
-                        <TabsTrigger value="customer">Customer</TabsTrigger>
-                        <TabsTrigger value="caterer">Caterer</TabsTrigger>
-                    </TabsList>
-
-                    <TabsContent value="customer">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Customer Login</CardTitle>
-                                <CardDescription>Sign in to book amazing catering services</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <LoginCustomer
-                                    form={form}
-                                    onSubmit={onSubmit}
-                                ></LoginCustomer>
-                            </CardContent>
-                        </Card>
-                    </TabsContent>
-
-                    <TabsContent value="caterer">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Caterer Login</CardTitle>
-                                <CardDescription>Access your catering business dashboard</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <LoginCaterer
-                                    form={form}
-                                    onSubmit={onSubmit}
-                                ></LoginCaterer>
-                            </CardContent>
-                        </Card>
-                    </TabsContent>
-                </Tabs>
-
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Customer Login</CardTitle>
+                        <CardDescription>Sign in to book amazing catering services</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <LoginCustomer
+                            form={form}
+                            onSubmit={onSubmit}
+                        ></LoginCustomer>
+                    </CardContent>
+                </Card>
                 <div className="mt-6 text-center">
-                    <p className="text-gray-600">
+                    <p className="text-gray-600 dark:text-white">
                         Don&apos;t have an account?
                         <Link href="/sign-up" className="text-primary hover:underline font-medium ml-2">
                             Sign up here
